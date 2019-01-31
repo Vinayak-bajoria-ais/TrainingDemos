c583fac62f1c442da57050e2e2b19a1895f62fe9
Vinayak <Vinayak.Bajoria@appliedis.com>
2019-01-31 11:59:50 +0530

    commit file

---
+++ a.ps1
@@ -0,0 +1,33 @@
+﻿try
+{
+"enter the first integer"
+[int]$a= Read-Host
+if(($a -ne [int]))
+{
+
+}
+else
+{
+throw  "a is not integer."
+}
+"enter the second integer"
+[int]$b= Read-Host
+if(($b -is [int]))
+{
+
+}
+elseif($b -eq 0)
+{
+throw "Arithmetic exception"
+}
+else
+{
+throw  "b is not integer."
+}
+[float]$c=$a/$b
+Write-Host $c
+}
+catch
+{
+Write-Host $_.Exception.Message
+}
